5de610a1c26116bfce0ca20968662f23e67e9dda
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-09-14 23:20:26 +0530

    chore: initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '2.4rem',
+    },
+    debugScreens: {
+      position: ['bottom', 'left'],
+      selector: '.debug-screens',
+      style: {
+        backgroundColor: 'black',
+        color: 'white',
+      },
+    },
+    screens: {
+      xxsm: '320px',
+      xsm: '425px',
+      mobile: '425px',
+      sm: '640px',
+      tablet: '640px',
+      md: '768px',
+      lg: '1024px',
+      laptop: '1024px',
+      xl: '1280px',
+      desktop: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('tailwindcss-debug-screens'),
+  ],
+}
