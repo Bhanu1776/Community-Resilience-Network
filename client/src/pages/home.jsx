9cfb313b4f29ff3c773a423c79920db64e4e831e
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-09-15 19:08:42 +0530

    Removed unused stuff

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,11 +1,9 @@
 export const Home = () => {
   return (
-    <div className="flex border-2 justify-center min-h-screen items-center ">
+    <div className="flex justify-center w-full min-h-screen items-center">
       <button
-        className=" relative top-0 left-0 bg-pink-700 hover:bg-pink-800 aspect-square p-20 rounded-full outline hover:outline-2 hover:outline-offset-2 hover:outline-neutral-800 hover:bg-pink-700 text-white text-4xl font-semibold tracking-wider duration-150 ease-in-out transition-all"
-        onClick={() => {
-          window.location.href = "/login";
-        }}
+        className=" relative top-0 left-0 bg-pink-700 hover:bg-pink-800 aspect-square p-20 rounded-full outline hover:outline-2 hover:outline-offset-2 hover:outline-neutral-800  text-white text-4xl font-semibold tracking-wider duration-150 ease-in-out transition-all"
+        onClick={() => {}}
       >
         Alert
         <span className=" absolute top-0 left-0 flex h-full w-full">
