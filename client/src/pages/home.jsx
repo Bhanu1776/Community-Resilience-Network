c36ac72bfcbc0da91e55f4492bee4f379e7676ca
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-09-15 13:20:48 +0530

    Ping AlertButn

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -2,12 +2,16 @@ export const Home = () => {
   return (
     <div className="flex border-2 justify-center min-h-screen items-center ">
       <button
-        className="bg-pink-500 p-20 rounded-full outline hover:outline-2 hover:outline-offset-2 hover:outline-neutral-800 hover:bg-pink-700 text-white font-bold"
+        className=" relative top-0 left-0 bg-pink-700 hover:bg-pink-800 aspect-square p-20 rounded-full outline hover:outline-2 hover:outline-offset-2 hover:outline-neutral-800 hover:bg-pink-700 text-white text-4xl font-semibold tracking-wider duration-150 ease-in-out transition-all"
         onClick={() => {
           window.location.href = "/login";
         }}
       >
-        Help
+        Alert
+        <span className=" absolute top-0 left-0 flex h-full w-full">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-10"></span>
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-100 opacity-75"></span>
+        </span>
       </button>
     </div>
   );
