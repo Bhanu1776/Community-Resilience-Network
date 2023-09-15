a43239a8ed1d9a9261993c17ef6b3b08139c0138
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-09-15 19:07:38 +0530

    Setup Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,54 @@
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom"
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import Navbar from "./components/Navbar";
 import { Home } from "./pages/home";
 import Location from "./pages/location/Location";
+import NotFound from "./pages/notFound/NotFound";
+import Login from "./pages/login/Login";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+
+const useAuth = () => {
+  const user = JSON.parse(localStorage.getItem('currentUser'));
+  if (user) {
+    return true;
+  }
+  return false;
+};
+
+const ProtectedRoutes = () => {
+  const auth = useAuth();
+
+  return auth ? <Outlet /> : <Navigate to="/login" />;
+};
 
 function App() {
+  const queryClient = new QueryClient()
+  const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
   return (
     <>
-      <Navbar />
-      {/* <Home /> */}
-      <Location/>
+      <Router>
+        <div className=" flex w-full h-screen">
+          <QueryClientProvider client={queryClient}>
+            <GoogleOAuthProvider clientId={clientId}>
+              <Navbar/>
+              <Routes>
+                <Route exact path="/" element={<Login />} />
+                <Route element={<ProtectedRoutes />}>
+                  <Route exact path='/home' element={< Home />} />
+                  <Route path='/location' element={<Location />} />
+                </Route>
+
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </GoogleOAuthProvider>
+          </QueryClientProvider>
+        </div>
+
+      </Router>
     </>
   );
 }
